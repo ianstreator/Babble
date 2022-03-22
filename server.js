83995bcc607b6ec56fc50b79ce33ba4fac607446
Ian Streator <istreator@yahoo.com>
2022-03-22 14:15:53 -0500

    moved socket script above js script attempt to fix socket issue

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use("/assets", express.static(path.join(__dirname, "dist", "assets")));
 //     res.sendFile(path.join(__dirname, "dist", "index.html"));
 //   });
 // });
-app.get(`/`, (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
