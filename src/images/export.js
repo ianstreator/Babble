409ea217566da53bce2e03d3e623d7a4ad3f12fa
Ian Streator <istreator@yahoo.com>
2022-04-06 10:53:53 -0500

    imported og image for meta tag

--- src/images/export.js
+++ src/images/export.js
@@ -2,5 +2,6 @@ import Logo from "./Babble-Logo.svg";
 import Exit from "./exit.svg";
 import Cloud from "./G-cloud.svg";
 import Github from "./github.svg"
+import babble_og from "./Babble-og.png"
 
-export default { Logo, Exit, Cloud, Github };
+export default { Logo, Exit, Cloud, Github, babble_og };
