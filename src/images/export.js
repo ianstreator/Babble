c675f44e49dec940d91fdcbf7e3848a762091fe6
Ian Streator <istreator@yahoo.com>
2022-05-18 16:21:14 -0500

    moved og:image to public directory

--- src/images/export.js
+++ src/images/export.js
@@ -2,7 +2,5 @@ import Logo from "./Babble-Logo.svg";
 import Exit from "./exit.svg";
 import Cloud from "./G-cloud.svg";
 import Github from "./github.svg";
-import babble_og from "./Babble-og.png";
-import babble_og2 from "./Babble-og(2).png";
 
-export default { Logo, Exit, Cloud, Github, babble_og, babble_og2 };
+export default { Logo, Exit, Cloud, Github };
